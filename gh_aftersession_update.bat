deb9b745dbce7d4b729b0776a5cc77368844e801
Daniel Möhring <daniel.moehring99@gmail.com>
2025-06-15 14:20:41 +0200

    Updated paths and made more resilient by adding trivial exits

--- gh_aftersession_update.bat
+++ gh_aftersession_update.bat
@@ -3,11 +3,11 @@ rem -----------------------------------------------------------
 rem  setup_gh_prereqs_windows_fixed.bat
 rem -----------------------------------------------------------
 
-set "DIRNAME=%USERPROFILE%\privat\DnD"
-set "FILENAME=Character - Faze Coremaker.pdf"
+set "DIRNAME=%USERPROFILE%\privat\DnD\GuildWars"
+set "FILENAME=Character Sheets\Character - Faze Coremaker.pdf"
 
 cd "%DIRNAME%"
-git pull
-git add "%DIRNAME%\%FILENAME%"
-git commit -m "After Session update"
-git push
+git pull || exit /b 1
+git add "%DIRNAME%\%FILENAME%" || exit /b 1
+git commit -m "After Session update" || exit /b 1
+git push || exit /b 1
